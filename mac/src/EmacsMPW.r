fe2b585a94593e6198f621402ee334787d87fb41
Eric Gallager <egall@gwmail.gwu.edu>
2023-11-12 12:37:35 -0500

    a few misc. things

--- mac/src/EmacsMPW.r
+++ mac/src/EmacsMPW.r
@@ -1,5 +1,5 @@
-/* Resource definitions for GNU Emacs on the Macintosh when building
-   under MPW.
+/* -*- Rez -*- Resource definitions for GNU Emacs on the Macintosh
+   when building under MPW.
 
    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004,
       2005, 2006, 2007  Free Software Foundation, Inc.
